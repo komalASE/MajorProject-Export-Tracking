d4ca984df47ac1e8c5df8a5e29fcdf0930f37c2e
komalASE <komal.kumari@habilelabs.io>
2023-01-31 00:38:15 +0530

    added server side validation

--- javascript/signUp.js
+++ javascript/signUp.js
@@ -1,39 +1,49 @@
 function formSubmission(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let username = document.getElementById("userName").value;
-    console.log("name -->", username);
-    let useremail = document.getElementById("userEmail").value;
-    let roleofperson = document.getElementById("userRole").value;
-    let userpassword = document.getElementById("userPassword").value;
+  let username = document.getElementById("userName").value;
+  let useremail = document.getElementById("userEmail").value;
+  let roleofperson = document.getElementById("userRole").value;
+  let userpassword = document.getElementById("userPassword").value;
 
+  const userdata = {
+    name: username,
+    email: useremail,
+    role: roleofperson,
+    password: userpassword
+  }
 
-    const userdata = {
-        name: username,
-        email: useremail,
-        role: roleofperson,
-        password: userpassword
-    }
-
-     // check if the localStorage already has some data
-  if(localStorage.getItem('userdata') === null){
+  // check if the localStorage already has some data
+  if (localStorage.getItem('userdata') === null) {
     let userData = []; // empty array
     // push new data
+
     userData.push(userdata);
     // set to localStorage
     localStorage.setItem('userdata', JSON.stringify(userData));
+    window.location.href = "../html/user-login.html"
+
   } else {
     // get existing data from localStorage
     let userData = JSON.parse(localStorage.getItem('userdata'));
-    // push new data
-    userData.push(userdata);
-    // reset localStorage
-    localStorage.setItem('userdata', JSON.stringify(userData));
-  }
 
-  alert("submitted successfully")
-  window.location.href = "../html/user-login.html"
+    if(userData.some((ele) => ele.email === useremail)){
+      alert("EMAIL ALREADY EXIST")
+    }
+    else{
+      userData.push(userdata);
+        localStorage.setItem('userdata', JSON.stringify(userData));
+        window.location.href = "../html/user-login.html"
+    }
+
+    // push new data
+    // userData.push(userdata);
+    // reset localStorage
+    // localStorage.setItem('userdata', JSON.stringify(userData));
+
+    // alert("submitted successfully")
+    // window.location.href = "../html/user-login.html"
+  }
 
 }
 
-  
